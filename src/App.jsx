60570ffb234b09d7e58504fee6e7ae06009bede7
Mauriciohnf45 <mh173509@gmail.com>
2024-10-19 08:51:19 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VerificarIdade from './components/VerificarIdade';
+
+function App() {
+  return (
+    <div className="App">
+      <VerificarIdade idade={20} />
+    </div>
+  );
+}
+
+export default App;
